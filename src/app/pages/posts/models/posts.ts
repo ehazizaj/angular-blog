57fca45e9a8d9734df6b0da9ce463bceee973c2a
Erjon Hazizaj <ehazizaj@optimalabania.com>
2020-05-11 13:20:41 +0200

    ️ 📊 added database

---
+++ src/app/pages/posts/models/posts.ts
@@ -0,0 +1,11 @@
+export interface Post {
+  id: number;
+  title: string;
+  subtitle: string;
+  image: string;
+  body: string;
+  postedBy: string;
+  postedOn: string;
+  read: boolean;
+  favourite: boolean;
+}
